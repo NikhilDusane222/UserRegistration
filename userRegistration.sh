3f63040199e22927857e0837ab859851196e0a06
Nikhil <you@example.com>
2020-03-29 00:14:44 +0530

    Added: Entered last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,9 +3,20 @@
 printf "Welcome to user registration problem \n"
 
 read -p "Enter the first name:" firstName
+# Pattern for first name
 patternForFirstName="^[A-Z]{1}[a-z]{2,}$"
 
 if [[ $firstName =~ $patternForFirstName ]]
+then
+	echo Valid
+else
+	echo Invalid
+fi
+read -p "Enter the last name:" lastName
+# Pattern for last name
+patternForLastName="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $lastName =~ $patternForLastName ]]
 then
 	echo Valid
 else
