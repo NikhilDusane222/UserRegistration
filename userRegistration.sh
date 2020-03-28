39564b7627d1375753fdcad9e156b84e59627bbc
Nikhil <you@example.com>
2020-03-29 00:12:04 +0530

    Added: Entered first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 printf "Welcome to user registration problem \n"
+
+read -p "Enter the first name:" firstName
+patternForFirstName="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $firstName =~ $patternForFirstName ]]
+then
+	echo Valid
+else
+	echo Invalid
+fi
