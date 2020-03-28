05401d9b08679d5c58527173246dbb61a91b4d9d
Nikhil <you@example.com>
2020-03-29 00:17:17 +0530

    Added: Entered valid email

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,24 +1,42 @@
 #!/bin/bash -x
 
-printf "Welcome to user registration problem \n"
-
-read -p "Enter the first name:" firstName
-# Pattern for first name
-patternForFirstName="^[A-Z]{1}[a-z]{2,}$"
-
-if [[ $firstName =~ $patternForFirstName ]]
-then
-	echo Valid
-else
-	echo Invalid
-fi
-read -p "Enter the last name:" lastName
-# Pattern for last name
-patternForLastName="^[A-Z]{1}[a-z]{2,}$"
-
-if [[ $lastName =~ $patternForLastName ]]
-then
-	echo Valid
-else
-	echo Invalid
-fi
+function validateFirstName()
+{
+	read -p "Enter the first name:" firstName
+	# Pattern for first name
+	patternForFirstName="^[A-Z]{1}[a-z]{2,}$"
+	if [[ $firstName =~ $patternForFirstName ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+}
+function validateLastName()
+{
+	read -p "Enter the last name:" lastName
+	# Pattern for last name
+	patternForLastName="^[A-Z]{1}[a-z]{2,}$"
+	if [[ $lastName =~ $patternForLastName ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+}
+function validateEmailId()
+{
+	read -p "Enter your email id:" emailId
+	patternForEmailId="^[a-zA-Z]+([.]?[+\-a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-z]{2,4}([.]?[a-z]{2,4})?$"
+	if [[ $emailId =~ $patternForEmailId ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+	echo $result
+}
+echo "Welcome to user registration problem "
+validateFirstName
+validateLastName
+validateEmailId
